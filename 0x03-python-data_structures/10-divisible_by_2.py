ff21c5702c9f5ff8acd907cfca6fd4adc3b0a823
zebiba <umutetu@gmail.com>
2023-03-13 11:29:52 -0700

    divis

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    xznew_div = []
+
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            new_div.append(True)
+        else:
+            new_div.append(False)
+
+    return (new_div)
