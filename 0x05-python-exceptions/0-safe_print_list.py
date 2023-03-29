6187f7e8efdae262f9a6cd5aaa3c673b7b8e483e
zebiba <umutetu@gmail.com>
2023-03-28 21:56:35 -0700

    list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-class Square:
-    """Class Square that defines a square.
-    """
-    pass
+def safe_print_list(my_list=[], x=0):
+    count = 0
+
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end='')
+        except:
+            break
+        else:
+            count += 1
+
+    print()
+    return (count)
